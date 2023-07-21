c31fc9185065400266f0c13f7ebab337590ef433
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-07-21 03:03:36 +0200

    commit_1

---
+++ src/pages/PageHome/PageHome.jsx
@@ -0,0 +1,22 @@
+import { authSelectors } from 'redux/auth';
+import {PageHomeSection, PageHomeTitle, PageHomeText, PageHomeTitle2 } from './PageHome.styled';
+
+const PageHome = () => {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+  return (
+    <>
+      <PageHomeSection>
+            <PageHomeTitle>Welcome 👋</PageHomeTitle>
+        <PageHomeTitle2>Now you will exactly not forget your contacts!</PageHomeTitle2>
+        {!isLoggedIn && (      
+          
+          <PageHomeText> Please, <b>Sign up</b> or <b>Log in</b> to have access to the
+            Phonebook!</PageHomeText>
+      )}
+      </PageHomeSection>
+      
+    </>
+  );
+}
+
+export default PageHome;
